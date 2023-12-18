570c11f99d11be7d8fffd51c638bbe815d8ef742
0-Elia-0 <126093891+0-Elia-0@users.noreply.github.com>
2023-12-18 16:42:53 +0100

    Navbar and homepage implementation

--- urbanhub/src/components/NavigationBar.tsx
+++ urbanhub/src/components/NavigationBar.tsx
@@ -1,9 +1,19 @@
-import { Navbar } from "react-bootstrap";
+import { Navbar, Container, Row } from "react-bootstrap";
 
 function NavigationBar() {
     return (
-        <Navbar bg="dark" variant="dark">
-            <Navbar.Brand href="/">UrbanHub</Navbar.Brand>
+        <Navbar expand="md" id="navbar">
+            <Container fluid className="px-md-5">
+                <Navbar.Brand href="/" style={{fontSize: '50px'}}>URBANHUB</Navbar.Brand>
+                <Navbar.Toggle/>
+                <Navbar.Collapse className="justify-content-end">
+                    <Row className="d-flex flex-row justify-content-center">
+                        <Navbar.Text className="text-center">
+                            Mario Rossi
+                        </Navbar.Text>
+                    </Row>
+                </Navbar.Collapse>
+            </Container>
         </Navbar>
     );
 }
--- urbanhub/src/pages/Homepage.tsx
+++ urbanhub/src/pages/Homepage.tsx
@@ -1,11 +1,31 @@
+import { Button, Container } from "react-bootstrap";
 import NavigationBar from "../components/NavigationBar";
+import { FaMapMarkedAlt, FaPlaneDeparture } from "react-icons/fa";
+import { Link } from "react-router-dom";
 
 function Homepage() {
     return (
       <>
       
       <NavigationBar/>
+
+      <Container className="d-flex flex-column justify-content-center align-items-center vh-100" style={{ position: 'absolute'}}>
       
+        <Link to='/newtrip' className="text-decoration-none text-white w-50">
+          <Button className="w-100 mb-5 homepageButton">
+            <FaPlaneDeparture className="me-2"/>
+            Start a new trip
+          </Button>
+        </Link>
+
+        <Link to='/trips' className="text-decoration-none text-white w-50">
+          <Button className="w-100 mb-5 homepageButton">
+            <FaMapMarkedAlt className="me-2"/>
+            See my trips
+          </Button>
+        </Link>
+
+      </Container>      
       </>
     );
   }
