8d81c2499198e211d3250ca5bd4d3fba43001966
SYLVIE MOLINATTO s318952 <s318952@studenti.polito.it>
2023-12-20 16:37:55 +0100

    adding trip creation structure and icon on the homepage

--- urbanhub/src/pages/Homepage.tsx
+++ urbanhub/src/pages/Homepage.tsx
@@ -1,16 +1,13 @@
 import { Button, Container } from "react-bootstrap";
-import NavigationBar from "../components/NavigationBar";
 import { FaMapMarkedAlt, FaPlaneDeparture } from "react-icons/fa";
 import { Link } from "react-router-dom";
 
 function Homepage() {
     return (
       <>
-      
-      <NavigationBar/>
-
-      <Container className="d-flex flex-column justify-content-center align-items-center vh-100" style={{ position: 'absolute'}}>
-      
+      <div className="homepage">
+      <Container className="d-flex flex-column justify-content-center align-items-center" >
+        <h1 className="title"> Inclusive and Effortless Urban Travel </h1>
         <Link to='/newtrip' className="text-decoration-none text-white w-50">
           <Button className="w-100 mb-5 homepageButton">
             <FaPlaneDeparture className="me-2"/>
@@ -25,7 +22,8 @@ function Homepage() {
           </Button>
         </Link>
 
-      </Container>      
+      </Container>   
+      </div>   
       </>
     );
   }
--- urbanhub/src/pages/NewTrip.tsx
+++ urbanhub/src/pages/NewTrip.tsx
@@ -1,7 +1,185 @@
-function NewTrip() {
-    return (
-        <>NewTripCreationPage made by different steps (destination - trip settings - trip preferences)</>
-    );
+import React, { useState } from 'react';
+import { Form, Button, Container, Row, Col } from 'react-bootstrap';
+import { Steps } from 'antd';
+
+const steps = [
+  {
+    title: "Trip destination",
+  },
+  {
+    title: "Trip settings",
+  },
+  {
+    title: "Trip preferences",
+  },
+  {
+    title: "Trip overview"
+  }
+];
+
+interface TripFormProps {
+  onSubmit: (data: {
+    destination: string;
+    startDate: string;
+    endDate: string;
+    adults: number;
+    kids: number;
+    budget: number;
+    additionalInfo: string;
+  }) => void;
 }
+
+const NewTrip: React.FC<TripFormProps> = ({ onSubmit }) => {
+  const [step, setStep] = useState(0);
+  const [formData, setFormData] = useState({
+    destination: '',
+    startDate: '',
+    endDate: '',
+    adults: 0,
+    kids: 0,
+    budget: 0,
+    additionalInfo: '',
+  });
+
+  const handleInputChange = (
+    e: React.ChangeEvent<HTMLInputElement | HTMLSelectElement | HTMLTextAreaElement>
+  ) => {
+    const { name, value } = e.target;
+    setFormData((prevData) => ({ ...prevData, [name]: value }));
+  };
+
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    if (step === 3) {
+      onSubmit(formData); // Submit the data only in the final step
+    }
+  };
+
+  const items = steps.map((item) => ({
+    key: item.title,
+    title: item.title,
+  }));
+
+  const nextStep = () => setStep((prevStep) => Math.min(prevStep + 1, 3));
+  const prevStep = () => setStep((prevStep) => Math.max(prevStep - 1, 0));
+
+  return (
+    <Container className="d-flex align-items-center justify-content-center" style={{ minHeight: '90vh' }}>
+      <Row>
+          <Col md={{ span: 12 }}>
+          {/* Ant Design Steps */}
+          <Steps current={step} items={items} size="small" className="mb-3">
+          </Steps>
+          <Form onSubmit={handleSubmit}>
+            <Form.Group controlId="Destination" hidden={step !== 0}>
+              <Form.Label>Destination</Form.Label>
+              <Form.Control
+                type="text"
+                name="destination"
+                placeholder="Enter destination"
+                value={formData.destination}
+                onChange={handleInputChange}
+              />
+            </Form.Group>
+
+            <Form.Group controlId="Trip settings" hidden={step !== 1}>
+              <Form.Group controlId="Start date">
+                <Form.Label>Start date</Form.Label>
+                <Form.Control
+                  type="date"
+                  name="startDate"
+                  value={formData.startDate}
+                  onChange={handleInputChange}
+                />
+                </Form.Group>
+              <Form.Group controlId="End date">
+                <Form.Label>End date</Form.Label>
+                <Form.Control
+                  type="date"
+                  name="endDate"
+                  value={formData.endDate}
+                  onChange={handleInputChange}
+                />
+              </Form.Group>
+              <Form.Group controlId="Number of adults">
+                <Form.Label>Number of adults</Form.Label>
+                <Form.Control
+                  type="number"
+                  name="adults"
+                  value={formData.adults}
+                  onChange={handleInputChange}
+                />
+                </Form.Group>
+              <Form.Group controlId="Number of kids">
+                <Form.Label>Number of kids</Form.Label>
+                <Form.Control
+                  type="number"
+                  name="kids"
+                  value={formData.kids}
+                  onChange={handleInputChange}
+                />
+              </Form.Group>
+              <Form.Group controlId="Budget">
+                <Form.Label>Budget</Form.Label>
+                <Form.Control
+                  type="number"
+                  name="budget"
+                  value={formData.budget}
+                  onChange={handleInputChange}
+                />
+              </Form.Group>
+            </Form.Group>
+
+            {/* Step 2 content goes here */}
+
+            <div hidden={step !== 3}>
+              <h4>Summary</h4>
+              <br />
+              <p>
+                <strong>Destination:</strong> {formData.destination}
+              </p>
+              <p>
+                <strong>Start Date:</strong> {formData.startDate}
+              </p>
+              <p>
+                <strong>End Date:</strong> {formData.endDate}
+              </p>
+              <p>
+                <strong>Number of Adults:</strong> {formData.adults}
+              </p>
+              <p>
+                <strong>Number of Kids:</strong> {formData.kids}
+              </p>
+              <p>
+                <strong>Budget:</strong> {formData.budget}
+              </p>
+              {/* You can add more fields as needed */}
+            </div>
+
+            <div className="mb-2 d-flex align-items-center justify-content-center">
+              {step > 0 && (
+                <Button variant="secondary" onClick={prevStep} className='button'>
+                  Previous
+                </Button>
+              )}
+
+              {step < 3 && (
+                <Button variant="primary" onClick={nextStep} className='button'>
+                  Next
+                </Button>
+              )}
+
+              {step === 3 && (
+                <Button variant="primary" type="submit" className='button'>
+                  Submit
+                </Button>
+              )}
+            </div>
+          </Form>
+        </Col>
+      </Row>
+    </Container>
+  );
+};
 
 export default NewTrip;
