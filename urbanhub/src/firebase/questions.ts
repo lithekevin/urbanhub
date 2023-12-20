bf9df3628be9ab657525b2fd2cd412f558eb0ca7
0-Elia-0 <126093891+0-Elia-0@users.noreply.github.com>
2023-12-20 16:44:23 +0100

    Create questions.ts

---
+++ urbanhub/src/firebase/questions.ts
@@ -0,0 +1,26 @@
+const questions: string[] = [
+
+    "Describe me your ideal trip.",
+    "Are you interested in historical landmarks and monuments?",
+    "Do you enjoy outdoor activities and nature?",
+    "Are you a food enthusiast? What type of cuisine do you prefer?",
+    "How do you feel about art and cultural experiences?",
+    "Are you looking for a relaxing or adventurous trip?",
+    "What is your preferred mode of transportation within the city?",
+    "Do you prefer a bustling city center or a quieter neighborhood?",
+    "Are you interested in local festivals and events?",
+    "How important is shopping to your travel experience?",
+    "Do you like sports? If so, what type of sports?",
+    "What is your preferred accommodation type (e.g., hotel, Airbnb, hostel)?",
+    "What kind of weather do you prefer?",
+    "Are you interested in guided tours or exploring on your own?",
+    "Why do you want to travel to this destination?",
+    "Do you have any specific hobbies or interests you'd like to pursue during the trip?",
+    "How important is nightlife and entertainment to your travel experience?",
+    "Are you open to trying new and adventurous activities?",
+    "What is your preferred season for traveling?",
+    "Do you have any specific health or dietary considerations that need to be taken into account?"
+];
+
+
+export default questions;
