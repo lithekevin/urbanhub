d1eb6c8a47dcfa3efb27d3b63a9fc4b395f71dc7
0-Elia-0 <126093891+0-Elia-0@users.noreply.github.com>
2023-12-21 16:49:25 +0100

    Small changes

--- urbanhub/src/pages/Homepage.tsx
+++ urbanhub/src/pages/Homepage.tsx
@@ -3,10 +3,11 @@ import { FaMapMarkedAlt, FaPlaneDeparture } from "react-icons/fa";
 import { Link } from "react-router-dom";
 
 function Homepage() {
+
     return (
       <>
-      <div className="homepage">
-      <Container className="d-flex flex-column justify-content-center align-items-center" >
+      <div className="homepage vh-100 vw-100 px-0">
+      <Container className="d-flex flex-column justify-content-center align-items-center w-100" >
         <h1 className="title"> Inclusive and Effortless Urban Travel </h1>
         <Link to='/newtrip' className="text-decoration-none text-white w-50">
           <Button className="w-100 mb-5 homepageButton">
