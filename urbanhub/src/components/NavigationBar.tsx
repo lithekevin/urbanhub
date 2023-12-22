5e89afbfa0c1b1d67aa8b4d30474a6f5089cac30
SYLVIE MOLINATTO s318952 <s318952@studenti.polito.it>
2023-12-22 11:34:15 +0100

    navigation bar changed

--- urbanhub/src/components/NavigationBar.tsx
+++ urbanhub/src/components/NavigationBar.tsx
@@ -1,22 +1,28 @@
-import { Navbar, Container, Row } from "react-bootstrap";
 import { PiUserCircleFill } from "react-icons/pi";
+import { Layout, Row, Col } from 'antd';
+
+const { Header } = Layout;
 
 function NavigationBar() {
   return (
-    <Navbar expand="md" id="navbar">
-      <Container fluid className="px-md-5">
-        <div className="d-flex align-items-center">
-          <img src="/smart-city.png" alt="logo" width="50" height="50" className="d-inline-block align-top"/>
-          <Navbar.Brand href="/" style={{ fontSize: '30px', marginLeft: '10px' }}>UrbanHub</Navbar.Brand>
-        </div>
-        <Row className="d-flex flex-row justify-content-center w-xs-100 w-md-auto">
-          <PiUserCircleFill style={{ fontSize: '35px', color: "black" }} />
-          <Navbar.Text className="text-center" style={{ padding: '0' }}>
-            Mario Rossi
-          </Navbar.Text>
-        </Row>
-      </Container>
-    </Navbar>
+    <Header className="header-style">
+      <Row justify="space-between" align="middle">
+        <Col>
+          <div className="d-flex align-items-center">
+            <img src="/smart-city.png" alt="logo" width="50" height="50" className="d-inline-block align-top"/>
+            <span className="projectName">UrbanHub</span>
+          </div>
+        </Col>
+        <Col>
+          <div className="d-flex align-items-center">
+            <PiUserCircleFill style={{ fontSize: '35px', color: "black" }} />
+            <span className="userInfo">
+              Mario Rossi
+            </span>
+          </div>
+        </Col>
+      </Row>
+    </Header>
   );
 }
 
