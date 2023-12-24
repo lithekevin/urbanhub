c38ada15ddff1ee515e34491b7dcfec675561c03
ALESSANDRO BIANCO <s317998@studenti.polito.it>
2023-12-24 19:48:45 +0100

    Added collapse element in trip overview page

--- urbanhub/src/pages/TripOverview.tsx
+++ urbanhub/src/pages/TripOverview.tsx
@@ -1,14 +1,17 @@
 import { Timeline } from 'antd';
+import type { CollapseProps } from 'antd';
+import { Collapse } from 'antd';
 import { GoogleMap, LoadScript, Marker } from '@react-google-maps/api';
 import { Container } from "react-bootstrap";
+import { useState } from 'react';
 
 //TODO: RICORDARSI DI METTERE DUE MODALITA' UNA READONLY E UNA EDITABLE
 
 
 function TripOverview() {
   const containerStyle = {
-    width: '800px',
-    height: '500px',
+    width: '100%',
+    height: '100%',
   };
   
   const center = {
@@ -26,35 +29,50 @@ function TripOverview() {
     );
   };
 
-    return (
-      <Container className="d-flex align-items-stretch content-padding-top">
-        <div style={{ flex: '0 0 25%' }}>
-          <Sidebar />
-        </div>
-        <div style={{ flex: '0 0 75%' }}>
-          <Container fluid className="position-relative d-flex flex-column align-items-center">
-            <h1 className="text-center" style={{ marginBottom: '30px' }}>TRIP OVERVIEW</h1>
+  return (<>
+    <h1 className="text-center" >TRIP OVERVIEW</h1>
+    <Container className="d-flex align-items-stretch content-padding-top" style={{ height: '100vh' }}>
+      <div style={{ flex: '0 0 25%', height: '100%' }}>
+        <Sidebar />
+      </div>
+      <div style={{ flex: '0 0 75%', height: '100%' }}>
+        <Container fluid className="position-relative d-flex flex-column align-items-center" style={{ height: '100%' }}>
+          <div style={{ width: '100%', height: '100%' }}>
             <GoogleMapComponent />
-          </Container>
-        </div>
-      </Container>
-    );
-  }
+          </div>
+        </Container>
+      </div>
+    </Container>
+    </>);
+};
+  
 
 function Sidebar() {
      //TODO: Invece di avere attrazioni fisse, caricarle da firebase
+
      const attractions = [
+        [{children: 'Attraction 1.1',}, {children: 'Attraction 1.2',}, {children: 'Attraction 1.3',}, {children: 'Attraction 1.4'}],
+        [{children: 'Attraction 2.1',}, {children: 'Attraction 2.2',}, {children: 'Attraction 2.3',}],
+        [{children: 'Attraction 3.1',}, {children: 'Attraction 3.2',}, {children: 'Attraction 3.3',}, {children: 'Attraction 3.4'}, {children: 'Attraction 3.5'}],
+      ];
+
+      const dayLabels = ["12/05/2024","13/05/2024","14/05/2024"];
+
+      const dailyActivities: CollapseProps['items'] = [
         {
-          children: 'Attraction 1',
+          key: '1',
+          label: dayLabels[0],
+          children: <Timeline items={attractions[0]} />,
         },
         {
-          children: 'Attraction 2',
+          key: '2',
+          label: dayLabels[1],
+          children: <Timeline items={attractions[1]} />,
         },
         {
-          children: 'Attraction 3',
-        },
-        {
-          children: 'Attraction 4',
+          key: '3',
+          label: dayLabels[2],
+          children: <Timeline items={attractions[2]} />,
         },
       ];
 
@@ -65,7 +83,7 @@ function Sidebar() {
         </Container>
       </div>
       <div>
-        <Timeline items={attractions} />
+      <Collapse size="large" items={dailyActivities} defaultActiveKey={['1']} accordion={true} />
       </div>
       </>);
   };
