9f891cd5cf7e1e7eab918424df8f68c8d8d668e2
0-Elia-0 <126093891+0-Elia-0@users.noreply.github.com>
2024-01-03 15:46:47 +0100

    Update NewTrip.tsx

--- urbanhub/src/pages/NewTrip.tsx
+++ urbanhub/src/pages/NewTrip.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Form, Button, DatePicker, InputNumber, Input, Steps, Row, Col, AutoComplete, ConfigProvider } from 'antd';
+import { Form, Button, DatePicker, InputNumber, Input, Steps, Row, Col, AutoComplete, ConfigProvider, Progress } from 'antd';
 import moment from 'moment';
 import questions from '../firebase/questions'; 
 import shuffle from 'lodash/shuffle';
@@ -64,6 +64,8 @@ const NewTrip: React.FC<TripFormProps> = () => {
 
   const [adultsValue, setAdultsValue] = React.useState<number>(0);
   const [kidsValue, setKidsValue] = React.useState<number>(0);
+
+  const [questionsPageNumber, setQuestionsPageNumber] = React.useState(0);
 
   // State to track the displayed questions
   const [displayedQuestions, setDisplayedQuestions] = React.useState<string[]>([]);
@@ -145,30 +147,107 @@ const NewTrip: React.FC<TripFormProps> = () => {
     setAllDisplayedQuestions(initialQuestions);
   }, []); 
 
+  // Load more questions when the user clicks the button
+  const loadMoreQuestions = () => {
+    const remainingQuestions = questions.filter(
+      (question) => !allDisplayedQuestions.includes(question)
+    );
+
+    const newQuestions = shuffle(remainingQuestions).slice(0, 3);
+
+    setDisplayedQuestions(() => [
+      ...newQuestions,
+    ]);
+
+    setAllDisplayedQuestions((prevAllDisplayedQuestions) => [
+      ...prevAllDisplayedQuestions,
+      ...newQuestions,
+    ]);
+
+    // Disable loading more questions if all questions are displayed
+    if (allDisplayedQuestions.length + newQuestions.length === 9) {
+      setCanLoadMoreQuestions(false);
+    }
+  };
+
+  console.log("displayed: ", displayedQuestions, "all: ", allDisplayedQuestions);
+
+  
+
   // Render random questions and text area fields for the third step
   const renderQuestions = () => {
     if (step === 2) {
-      return displayedQuestions.map((question, index) => (
-        <div key={index}>
-          <Row gutter={16}>
-            <Col span={24}>
-              <label className='label'>{question}</label>
-            </Col>
-            <Col span={24}>
-              <Form.Item
-                name={`answer${index}`}
-                hidden={step !== 2}
-              >
-                <Input.TextArea
-                  value={userAnswers[index]}
-                  onChange={(e) => handleAnswerChange(index, e.target.value)}
-                  rows={4}
-                />
-              </Form.Item>
+
+      return <>
+        <Row className='w-100 d-flex justify-content-center'>
+          <Col xs={{span:24}} className='w-100 d-flex flex-column justify-content-center align-items-center mb-4'>
+            <Progress className='w-75' percent={userAnswers.filter(a => a.length !== 0).length * 100/9}  showInfo={false} />
+            <small>{(userAnswers.filter(a => a.length !== 0).length * 100/9 < 100) ? "Keep answering questions until UrbanHub understand the perfect vacation style for you!" : "UrbanHub has understood your ideal vacation style. Click on next to confirm your choices and take a look at the results!"}</small>
+          </Col>
+
+        <div className='w-100'>
+          {
+            displayedQuestions.map((question, index) => (
+              <div key={index}>
+                <Row gutter={16}>
+                  <Col span={24}>
+                    <label className='label'>{question}</label>
+                  </Col>
+                  <Col span={24}>
+                    <Form.Item
+                      name={`answer${index + questionsPageNumber*3}`}
+                      hidden={step !== 2}
+                    >
+                      <Input.TextArea
+                        value={userAnswers[index + questionsPageNumber*3]}
+                        onChange={(e) => handleAnswerChange(index + questionsPageNumber*3, e.target.value)}
+                        rows={4}
+                      />
+                    </Form.Item>
+                  </Col>
+                </Row>
+              </div>
+            ))
+          }
+
+          </div>
+
+          <Row className='w-100'>
+            <Col span={24} className='d-flex flex-row justify-content-between'>
+              <Button onClick={() => {
+
+                setDisplayedQuestions(allDisplayedQuestions.slice((questionsPageNumber-1)*3, (questionsPageNumber-1)*3+3))
+
+                setQuestionsPageNumber((number) => number-1)
+                
+                }} disabled={questionsPageNumber === 0}>{"<"}</Button>
+              {
+                questionsPageNumber < 2 &&
+                
+                <Button onClick={() => {
+
+                  if(allDisplayedQuestions.length === questionsPageNumber*3+3){
+                    loadMoreQuestions();
+                  }
+                  else{
+
+                    setDisplayedQuestions(allDisplayedQuestions.slice((questionsPageNumber+1)*3, (questionsPageNumber+1)*3+3))
+
+                  }
+  
+                  setQuestionsPageNumber((number) => number+1)
+                  
+                  
+  
+                  }} disabled={questionsPageNumber === 2 || userAnswers.slice(questionsPageNumber*3, questionsPageNumber*3+3).length < 3 || userAnswers.slice(questionsPageNumber*3, questionsPageNumber*3+3).some((ans) => ans.length === 0)}>{">"}</Button>
+              
+              }
+              
             </Col>
           </Row>
-        </div>
-      ));
+
+        </Row>
+      </>
     }
     else if (step === 3){
       return allDisplayedQuestions.map((question, index) => (
@@ -186,30 +265,6 @@ const NewTrip: React.FC<TripFormProps> = () => {
     }
     else{
       return null;
-    }
-  };
-
-  // Load more questions when the user clicks the button
-  const loadMoreQuestions = () => {
-    const remainingQuestions = questions.filter(
-      (question) => !allDisplayedQuestions.includes(question)
-    );
-
-    const newQuestions = shuffle(remainingQuestions).slice(0, 3);
-
-    setDisplayedQuestions((prevDisplayedQuestions) => [
-      ...prevDisplayedQuestions,
-      ...newQuestions,
-    ]);
-
-    setAllDisplayedQuestions((prevAllDisplayedQuestions) => [
-      ...prevAllDisplayedQuestions,
-      ...newQuestions,
-    ]);
-
-    // Disable loading more questions if all questions are displayed
-    if (allDisplayedQuestions.length + newQuestions.length === 9) {
-      setCanLoadMoreQuestions(false);
     }
   };
 
