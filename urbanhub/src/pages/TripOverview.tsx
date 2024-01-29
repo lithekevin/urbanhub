1047c17cabdea9014905ce58f6293bdad674775a
lithekevin <130843364+lithekevin@users.noreply.github.com>
2024-01-29 18:17:31 +0100

    Change to Form layout

--- urbanhub/src/pages/TripOverview.tsx
+++ urbanhub/src/pages/TripOverview.tsx
@@ -393,101 +393,102 @@ function TripOverview(props: any) {
   
     return (
       <>
-        <Modal open={isFormVisible} onCancel={closeForm} footer={null} centered>
-          <Form form={form} name={"formName"} onFinish={(values) => onFinish(values)}>
+        <Modal open={isFormVisible} onCancel={closeForm} footer={null} centered width={1000}>
+          <Form form={form} name={"formName"} layout= "vertical" onFinish={(values) => onFinish(values)}>
           <Title level={2} className='step-title'> {editingAttraction ? "Edit Attraction" : "Add Attraction"} </Title>
-          <Paragraph className='label'> Attraction: </Paragraph>
-          <Form.Item name="attraction" style={{paddingTop: '10px'}} rules={[{ required: true, message: 'Please select one of the attractions in the map, or type the name!' }]}>
-            <AutoComplete
-              options={cities.find(city => city.name === trip?.city)?.attractions.map(attraction => ({ value: attraction.name}))}
-              placeholder="Type an attraction"
-              style={{width: '100%'}}
-              allowClear={{ clearIcon: <CloseSquareFilled /> }}
-              onClear={() => {
-                form.setFieldsValue({ attraction: '' });
-                setValidSelection(false);
-                setSelectedAttractionId(null);
-              }}
-              filterOption={(inputValue, option) =>
-                option?.value.toUpperCase().indexOf(inputValue.toUpperCase()) !== -1
-              }
-              onSelect={(value) => {
-                const selectedAttraction = cities.find(city => city.name === trip?.city)?.attractions.find(attraction => attraction.name === value);
-                if(selectedAttraction) {
-                  setSelectedAttractionId(selectedAttraction.id);
-                  setValidSelection(true);
-                }
-              }}
-              onBlur={() => {
-                if (form.getFieldValue('attraction') !== selAttraction?.name) {
-                  form.setFieldsValue({ attraction: '' });
-                  setValidSelection(false);
-                  setSelectedAttractionId(null);
-                  form.validateFields(['attraction']);
-                }                
-              }}
+          <Row>
+            <Col>
+              <Form.Item name="attraction" label="Attraction" style={{paddingTop: '10px'}} rules={[{ required: true, message: 'Please select one of the attractions in the map, or type the name!' }]}>
+                <AutoComplete
+                  options={cities.find(city => city.name === trip?.city)?.attractions.map(attraction => ({ value: attraction.name}))}
+                  placeholder="Type an attraction"
+                  style={{width: '100%'}}
+                  allowClear={{ clearIcon: <CloseSquareFilled /> }}
+                  onClear={() => {
+                    form.setFieldsValue({ attraction: '' });
+                    setValidSelection(false);
+                    setSelectedAttractionId(null);
+                  }}
+                  filterOption={(inputValue, option) =>
+                    option?.value.toUpperCase().indexOf(inputValue.toUpperCase()) !== -1
+                  }
+                  onSelect={(value) => {
+                    const selectedAttraction = cities.find(city => city.name === trip?.city)?.attractions.find(attraction => attraction.name === value);
+                    if(selectedAttraction) {
+                      setSelectedAttractionId(selectedAttraction.id);
+                      setValidSelection(true);
+                    }
+                  }}
+                  onBlur={() => {
+                    if (form.getFieldValue('attraction') !== selAttraction?.name) {
+                      form.setFieldsValue({ attraction: '' });
+                      setValidSelection(false);
+                      setSelectedAttractionId(null);
+                      form.validateFields(['attraction']);
+                    }                
+                  }}
+                  
+                />
+              </Form.Item>
+              {selectedAttractionId && validSelection && cities.find(city => city.name === trip?.city)!.attractions.find(attraction => attraction.id === selectedAttractionId) && 
               
-            />
-          </Form.Item>
-          {selectedAttractionId && validSelection && cities.find(city => city.name === trip?.city)!.attractions.find(attraction => attraction.id === selectedAttractionId) && 
-          
-            <p style={{color: "var(--hard-background-color)"}}>This attraction will add a cost of {cities.find(city => city.name === trip?.city)!.attractions.find(attraction => attraction.id === selectedAttractionId)!.perPersonCost * (trip!.nAdults + trip!.nKids)}{" € to your trip."}</p>
+                <p style={{color: "var(--hard-background-color)"}}>This attraction will add a cost of {cities.find(city => city.name === trip?.city)!.attractions.find(attraction => attraction.id === selectedAttractionId)!.perPersonCost * (trip!.nAdults + trip!.nKids)}{" € to your trip."}</p>
 
-          }
-          <Paragraph className='label'> Date: </Paragraph>
-            <Form.Item name="date" rules={[{ required: true, message: 'Please choose the date!' }]}>
-              <DatePicker format="DD/MM/YYYY" disabledDate={(current) => current && current < moment().startOf('day')} style={{width: '100%'}}/>
-            </Form.Item>
-            <Form.Item style={{ marginBottom: 0 }}>
-              <Form.Item label = "Start Time " name="startTime" style={{ display: 'inline-block', width: 'calc(50% - 8px)', marginRight: '16px' }} rules={[{ required: true, message: 'Please choose the start time!' }]}>
-                <TimePicker format="HH:mm" />
-              </Form.Item>
-              <Form.Item label= "End Time " name="endTime" style={{ display: 'inline-block', width: 'calc(50% - 8px)' }} rules={[{ required: true, message: 'Please choose the end time!' }]}>
-                <TimePicker format="HH:mm" />
-              </Form.Item>
-            </Form.Item>
-            <Form.Item>
-              <Row>
-                <Col>
-                <GoogleMap clickableIcons={false} mapContainerStyle={{ width: '100%', height: '40vh', margin: 'auto', display: 'block', borderRadius: '10px', boxShadow: '0 8px 16px rgba(0, 0, 0, 0.1)' }} center={selectedAttractionId === null ? cityPosition : cityPosition } zoom={zoomLevel} onLoad={(map) => {setMap(map)}}>             
-                  { !selectedAttractionId && (
-                    <>
-                    {cities.find(city => city.name === trip?.city)?.attractions.map((attraction: Attraction, index: number) => {
-                      return (
-                        <Marker key={attraction.id} position={{ lat: attraction.location.latitude, lng: attraction.location.longitude }} onMouseOver={() => setSelectedMarker(attraction)} onMouseOut={() => setSelectedMarker(null)}  onClick={() => {
-                          setSelectedAttractionId(attraction.id);
-                          form.setFieldsValue({ attraction: attraction.name }); // Update the AutoComplete value
-                        }}/>
-                      );
-                    })}
-                  </>
-                  )}
-                  { selectedAttractionId && (<>
-                    {cities.find(city => city.name === trip?.city)?.attractions.map((attraction: Attraction) => {
-                      if (attraction.id === selectedAttractionId) {
+              }
+                <Form.Item name="date" label= "Date" rules={[{ required: true, message: 'Please choose the date!' }]}>
+                  <DatePicker format="DD/MM/YYYY" disabledDate={(current) => current && current < moment().startOf('day')} style={{width: '100%'}}/>
+                </Form.Item>
+                <Form.Item style={{ marginBottom: 0 }}>
+                  <Form.Item label = "Start Time " name="startTime" style={{ display: 'inline-block', marginRight: "2vw"}} rules={[{ required: true, message: 'Please choose the start time!' }]}>
+                    <TimePicker format="HH:mm" />
+                  </Form.Item>
+                  <Form.Item label= "End Time" name="endTime" style={{ display: 'inline-block' }} rules={[{ required: true, message: 'Please choose the end time!' }]}>
+                    <TimePicker format="HH:mm" />
+                  </Form.Item>
+                </Form.Item>
+            </Col>
+            <Col>
+                <Form.Item>
+                
+                  <GoogleMap clickableIcons={false} mapContainerStyle={{ width: '100%', height: '40vh', margin: 'auto', display: 'block', borderRadius: '10px', boxShadow: '0 8px 16px rgba(0, 0, 0, 0.1)' }} center={selectedAttractionId === null ? cityPosition : cityPosition } zoom={zoomLevel} onLoad={(map) => {setMap(map)}}>             
+                    { !selectedAttractionId && (
+                      <>
+                      {cities.find(city => city.name === trip?.city)?.attractions.map((attraction: Attraction, index: number) => {
                         return (
-                          <Marker key={attraction.id} position={{ lat: attraction.location.latitude, lng: attraction.location.longitude }} onMouseOver={() => setSelectedMarker(attraction)} onMouseOut={() => setSelectedMarker(null)}/>
+                          <Marker key={attraction.id} position={{ lat: attraction.location.latitude, lng: attraction.location.longitude }} onMouseOver={() => setSelectedMarker(attraction)} onMouseOut={() => setSelectedMarker(null)}  onClick={() => {
+                            setSelectedAttractionId(attraction.id);
+                            form.setFieldsValue({ attraction: attraction.name }); // Update the AutoComplete value
+                          }}/>
                         );
-                      }
-                        return null; // Render nothing if the attraction is not the selected one
                       })}
-                  </>)}
-                  {selectedMarker && (
-                    <InfoWindow
-                    options={{ pixelOffset: new google.maps.Size(0, -35), disableAutoPan: true }}
-                    position={{ lat: selectedMarker.location.latitude, lng: selectedMarker.location.longitude }}
-                    onCloseClick={() => {setSelectedMarker(null)}}
-                  >
-                    <div className="attractionContainer">
-                      <img className="attractionImage" src={imageUrl || defaultAttractionImageUrl} alt={selectedMarker.name} />
-                      <h6 className="attractionName">{selectedMarker.name}</h6>
-                    </div>
-                  </InfoWindow>
-                  )}
-                </GoogleMap>
-                </Col>
-              </Row>
-            </Form.Item>
+                    </>
+                    )}
+                    { selectedAttractionId && (<>
+                      {cities.find(city => city.name === trip?.city)?.attractions.map((attraction: Attraction) => {
+                        if (attraction.id === selectedAttractionId) {
+                          return (
+                            <Marker key={attraction.id} position={{ lat: attraction.location.latitude, lng: attraction.location.longitude }} onMouseOver={() => setSelectedMarker(attraction)} onMouseOut={() => setSelectedMarker(null)}/>
+                          );
+                        }
+                          return null; // Render nothing if the attraction is not the selected one
+                        })}
+                    </>)}
+                    {selectedMarker && (
+                      <InfoWindow
+                      options={{ pixelOffset: new google.maps.Size(0, -35), disableAutoPan: true }}
+                      position={{ lat: selectedMarker.location.latitude, lng: selectedMarker.location.longitude }}
+                      onCloseClick={() => {setSelectedMarker(null)}}
+                    >
+                      <div className="attractionContainer">
+                        <img className="attractionImage" src={imageUrl || defaultAttractionImageUrl} alt={selectedMarker.name} />
+                        <h6 className="attractionName">{selectedMarker.name}</h6>
+                      </div>
+                    </InfoWindow>
+                    )}
+                  </GoogleMap>
+              </Form.Item>
+            </Col>
+          </Row>
             <Form.Item style={{ display: 'flex', justifyContent: 'flex-end' }}>
                 <Button onClick={closeForm} style={{ marginRight: '10px' }}>
                   Cancel
