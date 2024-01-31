cff0572e060118e33b6d631949422b4ba80d02c9
SYLVIE MOLINATTO s318952 <s318952@studenti.polito.it>
2024-01-31 18:51:32 +0100

    fix little bug

--- urbanhub/src/pages/TripOverview.tsx
+++ urbanhub/src/pages/TripOverview.tsx
@@ -395,58 +395,60 @@ function TripOverview(props: any) {
       <>
         <Modal open={isFormVisible} onCancel={closeForm} footer={null} centered width={1000}>
           <Form form={form} name={"formName"} layout= "vertical" onFinish={(values) => onFinish(values)}>
-          <Title level={2} className='step-title'> {editingAttraction ? "Edit Attraction" : "Add Attraction"} </Title>
-          <Row>
-            <Col>
-              <Form.Item name="attraction" label="Attraction" style={{paddingTop: '10px'}} rules={[{ required: true, message: 'Please select one of the attractions in the map, or type the name!' }]}>
-                <AutoComplete
-                  options={cities.find(city => city.name === trip?.city)?.attractions.map(attraction => ({ value: attraction.name}))}
-                  placeholder="Type an attraction"
-                  style={{width: '100%'}}
-                  allowClear={{ clearIcon: <CloseSquareFilled /> }}
-                  onClear={() => {
-                    form.setFieldsValue({ attraction: '' });
-                    setValidSelection(false);
-                    setSelectedAttractionId(null);
-                  }}
-                  filterOption={(inputValue, option) =>
-                    option?.value.toUpperCase().indexOf(inputValue.toUpperCase()) !== -1
-                  }
-                  onSelect={(value) => {
-                    const selectedAttraction = cities.find(city => city.name === trip?.city)?.attractions.find(attraction => attraction.name === value);
-                    if(selectedAttraction) {
-                      setSelectedAttractionId(selectedAttraction.id);
-                      setValidSelection(true);
-                    }
-                  }}
-                  onBlur={() => {
-                    if (form.getFieldValue('attraction') !== selAttraction?.name) {
+            <Title level={2} className='step-title'> {editingAttraction ? "Edit Attraction" : "Add Attraction"} </Title>
+            <Row>
+              <Col>
+                <Form.Item name="attraction" label="Attraction" style={{paddingTop: '10px'}} rules={[{ required: true, message: 'Please select one of the attractions in the map, or type the name!' }]}>
+                  <div>
+                  <AutoComplete
+                    options={cities.find(city => city.name === trip?.city)?.attractions.map(attraction => ({ value: attraction.name}))}
+                    placeholder="Type an attraction"
+                    style={{width: '100%'}}
+                    allowClear={{ clearIcon: <CloseSquareFilled /> }}
+                    onClear={() => {
                       form.setFieldsValue({ attraction: '' });
                       setValidSelection(false);
                       setSelectedAttractionId(null);
-                      form.validateFields(['attraction']);
-                    }                
-                  }} 
-                />
-                {selectedAttractionId && validSelection && cities.find(city => city.name === trip?.city)!.attractions.find(attraction => attraction.id === selectedAttractionId) && 
-                <Paragraph style={{color: "var(--hard-background-color)", marginBottom: '0', marginTop: '0.4%'}}>This attraction will add a cost of {cities.find(city => city.name === trip?.city)!.attractions.find(attraction => attraction.id === selectedAttractionId)!.perPersonCost * (trip!.nAdults + trip!.nKids)}{" € to your trip."}</Paragraph>
-                }
-              </Form.Item>
-              <Form.Item name="date" label= "Date" rules={[{ required: true, message: 'Please choose the date!' }]}>
-                <DatePicker format="DD/MM/YYYY" disabledDate={(current) => current && current < moment().startOf('day')} style={{width: '100%'}}/>
-              </Form.Item>
-              <Form.Item style={{ marginBottom: 0 }}>
-                <Form.Item label = "Start Time " name="startTime" style={{ display: 'inline-block', marginRight: "2vw"}} rules={[{ required: true, message: 'Please choose the start time!' }]}>
-                  <TimePicker format="HH:mm" />
+                    }}
+                    filterOption={(inputValue, option) =>
+                      option?.value.toUpperCase().indexOf(inputValue.toUpperCase()) !== -1
+                    }
+                    onSelect={(value) => {
+                      const selectedAttraction = cities.find(city => city.name === trip?.city)?.attractions.find(attraction => attraction.name === value);
+                      if(selectedAttraction) {
+                        setSelectedAttractionId(selectedAttraction.id);
+                        setValidSelection(true);
+                        form.setFieldsValue({ attraction: selectedAttraction.name });
+                      }
+                    }}
+                    onBlur={() => {
+                      if (form.getFieldValue('attraction') !== selAttraction?.name) {
+                        form.setFieldsValue({ attraction: '' });
+                        setValidSelection(false);
+                        setSelectedAttractionId(null);
+                        form.validateFields(['attraction']);
+                      }                
+                    }} 
+                  />
+                  {selectedAttractionId && validSelection && cities.find(city => city.name === trip?.city)!.attractions.find(attraction => attraction.id === selectedAttractionId) && 
+                  <Paragraph style={{color: "var(--hard-background-color)", marginTop: '0.4%', marginBottom: '0'}}>This attraction will add a cost of {cities.find(city => city.name === trip?.city)!.attractions.find(attraction => attraction.id === selectedAttractionId)!.perPersonCost * (trip!.nAdults + trip!.nKids)}{" € to your trip."}</Paragraph>
+                  }
+                  </div>
                 </Form.Item>
-                <Form.Item label= "End Time" name="endTime" style={{ display: 'inline-block' }} rules={[{ required: true, message: 'Please choose the end time!' }]}>
-                  <TimePicker format="HH:mm" />
+                <Form.Item name="date" label= "Date" rules={[{ required: true, message: 'Please choose the date!' }]}>
+                  <DatePicker format="DD/MM/YYYY" disabledDate={(current) => current && current < moment().startOf('day')} style={{width: '100%'}}/>
                 </Form.Item>
-              </Form.Item>
-            </Col>
-            <Col>
+                <Form.Item style={{ marginBottom: 0 }}>
+                  <Form.Item label = "Start Time " name="startTime" style={{ display: 'inline-block', marginRight: "2vw"}} rules={[{ required: true, message: 'Please choose the start time!' }]}>
+                    <TimePicker format="HH:mm" />
+                  </Form.Item>
+                  <Form.Item label= "End Time" name="endTime" style={{ display: 'inline-block' }} rules={[{ required: true, message: 'Please choose the end time!' }]}>
+                    <TimePicker format="HH:mm" />
+                  </Form.Item>
+                </Form.Item>
+              </Col>
+              <Col>
                 <Form.Item>
-                
                   <GoogleMap clickableIcons={false} mapContainerStyle={{ width: '100%', height: '40vh', margin: 'auto', display: 'block', borderRadius: '10px', boxShadow: '0 8px 16px rgba(0, 0, 0, 0.1)' }} center={selectedAttractionId === null ? cityPosition : cityPosition } zoom={zoomLevel} onLoad={(map) => {setMap(map)}}>             
                     { !selectedAttractionId && (
                       <>
@@ -483,16 +485,16 @@ function TripOverview(props: any) {
                     </InfoWindow>
                     )}
                   </GoogleMap>
-              </Form.Item>
-            </Col>
-          </Row>
+                </Form.Item>
+              </Col>
+            </Row>
             <Form.Item style={{ display: 'flex', justifyContent: 'flex-end' }}>
-                <Button onClick={closeForm} style={{ marginRight: '10px' }}>
-                  Cancel
-                </Button>
-                <Button type="primary" htmlType="submit">
-                  Submit
-                </Button>
+              <Button onClick={closeForm} style={{ marginRight: '10px' }}>
+                Cancel
+              </Button>
+              <Button type="primary" htmlType="submit">
+                Submit
+              </Button>
             </Form.Item>
           </Form>
         </Modal>
