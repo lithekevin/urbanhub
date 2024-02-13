e6f3aef0ba085b468488548fd1984de27d28cbf8
0-Elia-0 <126093891+0-Elia-0@users.noreply.github.com>
2024-02-13 16:34:03 +0100

    Code formatting

--- urbanhub/src/components/TripOverview/GoogleMapsComponent.tsx
+++ urbanhub/src/components/TripOverview/GoogleMapsComponent.tsx
@@ -1,23 +1,29 @@
-import { GoogleMap, Marker, DirectionsRenderer,InfoWindow } from '@react-google-maps/api';
-import dayjs from 'dayjs';
-import React, { SetStateAction, useEffect, useState } from 'react';
-import { TripAttraction } from '../../models/tripAttraction';
-import { Trip } from '../../models/trip';
-import { Attraction } from '../../models/attraction';
-import { EuroCircleOutlined } from "@ant-design/icons"
-import axios from 'axios';
-import { Image, Tag, Typography } from 'antd';
-import cities from '../../firebase/cities';
+import {
+  GoogleMap,
+  Marker,
+  DirectionsRenderer,
+  InfoWindow,
+} from "@react-google-maps/api";
+import dayjs from "dayjs";
+import React, { SetStateAction, useEffect, useState } from "react";
+import { TripAttraction } from "../../models/tripAttraction";
+import { Trip } from "../../models/trip";
+import { Attraction } from "../../models/attraction";
+import { EuroCircleOutlined } from "@ant-design/icons";
+import axios from "axios";
+import { Image, Tag, Typography } from "antd";
+import cities from "../../firebase/cities";
 
 const { Title } = Typography;
 
-const defaultAttractionImageUrl = "https://images.unsplash.com/photo-1416397202228-6b2eb5b3bb26?q=80&w=1167&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+const defaultAttractionImageUrl =
+  "https://images.unsplash.com/photo-1416397202228-6b2eb5b3bb26?q=80&w=1167&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D";
 
 interface CityPosition {
   lat: number;
   lng: number;
 }
-  
+
 interface DirectionsState {
   geocoded_waypoints: any[]; // You might want to create a more specific type for this
   routes: any[]; // You might want to create a more specific type for this
@@ -51,9 +57,15 @@ interface GoogleMapsComponentProps {
   };
 }
 
-function GoogleMapsComponent(props : GoogleMapsComponentProps) {
-  
-  const { activeKeyState, cityPositionState, defaultCenter, directionsState, tripState, attractionCardHoveredID } = props;
+function GoogleMapsComponent(props: GoogleMapsComponentProps) {
+  const {
+    activeKeyState,
+    cityPositionState,
+    defaultCenter,
+    directionsState,
+    tripState,
+    attractionCardHoveredID,
+  } = props;
 
   const [imageUrl, setImageUrl] = useState(null);
   const [imageLoading, setImageLoading] = useState(false);
@@ -62,22 +74,25 @@ function GoogleMapsComponent(props : GoogleMapsComponentProps) {
   const fetchImage = async (query: string) => {
     setImageLoading(true);
     try {
-      const response = await axios.get('https://api.unsplash.com/search/photos', {
-        params: {
-          query,
-          per_page: 1,
-        },
-        headers: {
-          Authorization: `Client-ID 4rjvZvwzFuPY3uX3WAnf2Qb8eWkwvDys-sdsyvDdai0`,
-        },
-      });
-  
+      const response = await axios.get(
+        "https://api.unsplash.com/search/photos",
+        {
+          params: {
+            query,
+            per_page: 1,
+          },
+          headers: {
+            Authorization: `Client-ID 4rjvZvwzFuPY3uX3WAnf2Qb8eWkwvDys-sdsyvDdai0`,
+          },
+        }
+      );
+
       if (response.data.results.length > 0) {
         setImageLoading(false);
         return response.data.results[0].urls.regular;
       } else {
         setImageLoading(false);
-        console.log('No results from unsplash')
+        console.log("No results from unsplash");
         return null;
       }
     } catch (error) {
@@ -86,94 +101,187 @@ function GoogleMapsComponent(props : GoogleMapsComponentProps) {
       return null;
     }
   };
-  
+
   useEffect(() => {
     if (selectedMarker) {
-      fetchImage(selectedMarker.name + " " + tripState.value?.city).then(setImageUrl);
+      fetchImage(selectedMarker.name + " " + tripState.value?.city).then(
+        setImageUrl
+      );
     }
-  }, [selectedMarker]);  
-  
+  }, [selectedMarker]);
+
   useEffect(() => {
-    if(activeKeyState.value.length === 0){
+    if (activeKeyState.value.length === 0) {
       setSelectedMarker(null);
-      return
+      return;
     }
 
     setSelectedMarker(null);
-  }, [activeKeyState.value.length, activeKeyState.value[0]])
+  }, [activeKeyState.value.length, activeKeyState.value[0]]);
 
   const renderMarkerForDay = (day: dayjs.Dayjs) => {
     let attractionsForDay: TripAttraction[] = [];
-  
+
     // Find the closest matching key
     let closestKey: dayjs.Dayjs | null = null;
     let minDifference: number | null = null;
-  
+
     tripState.value?.schedule.forEach((attractions, key) => {
-      const difference = Math.abs(day.diff(key, 'days'));
-  
+      const difference = Math.abs(day.diff(key, "days"));
+
       if (minDifference === null || difference < minDifference) {
         minDifference = difference;
         closestKey = key;
       }
     });
-  
+
     if (closestKey !== null) {
       attractionsForDay = tripState.value?.schedule.get(closestKey) || [];
     }
     return attractionsForDay;
   };
-  
-  const dayLabels = Array.from(tripState.value?.schedule.keys() || []).map((day) => day.format('DD/MM/YYYY'));
+
+  const dayLabels = Array.from(tripState.value?.schedule.keys() || []).map(
+    (day) => day.format("DD/MM/YYYY")
+  );
 
   const zoomLevel = activeKeyState.value.length === 1 ? 15 : 10;
 
   useEffect(() => {
     console.log(attractionCardHoveredID.value);
     if (attractionCardHoveredID.value) {
-      const attraction = cities.find((city) => city.name === tripState.value?.city)?.attractions.find((attraction) => attraction.id === attractionCardHoveredID.value);
+      const attraction = cities
+        .find((city) => city.name === tripState.value?.city)
+        ?.attractions.find(
+          (attraction) => attraction.id === attractionCardHoveredID.value
+        );
       if (attraction) {
         setSelectedMarker(attraction);
       }
-    }
-    else {
+    } else {
       setSelectedMarker(null);
     }
   }, [attractionCardHoveredID.value]);
 
-  return(
+  return (
     <>
-      <GoogleMap clickableIcons={false} mapContainerStyle={{ width: '100%', height: '65vh', borderRadius: '10px', boxShadow: '0 8px 16px rgba(0, 0, 0, 0.1)' }} center={activeKeyState.value.length === 0 ? cityPositionState.value : directionsState.value?.routes[0]?.legs[0]?.start_location } zoom={zoomLevel} onLoad={(map) => {}}>
-        {(cityPositionState.value.lat !== defaultCenter.lat && cityPositionState.value.lng !== defaultCenter.lng && activeKeyState.value.length === 0 && <Marker position={cityPositionState.value} icon={{ url: "https://imgur.com/HXGfoxe.png", scaledSize: new window.google.maps.Size(38, 38) }}/>)}             
-        {activeKeyState.value.length === 1 && <DirectionsRenderer directions={directionsState.value} options={{ suppressMarkers: true }}/>}
-        {cityPositionState.value.lat !== defaultCenter.lat && cityPositionState.value.lng !== defaultCenter.lng && activeKeyState.value.length > 0 && (
-          <>
-            {renderMarkerForDay(dayjs(dayLabels[parseInt(activeKeyState.value[0], 10)], 'DD/MM/YYYY')).map((attraction: Attraction, index: number) => {              return (
-                <Marker 
-                  key={`${attraction.id}-${index}`} 
-                  position={{ lat: attraction.location.latitude, lng: attraction.location.longitude }} 
-                  label={{text:`${(index + 1).toString()}`,color:'white', fontWeight: 'bold'}} 
-                  icon={{ url: 'https://imgur.com/gfrQTuH.png', scaledSize: new google.maps.Size(38, 38), labelOrigin: new google.maps.Point(19, 16)}}
-                  onClick={() => {selectedMarker === null ? setSelectedMarker(attraction) : setSelectedMarker(null)}}/>
-              );
-            })}
-          </>
+      <GoogleMap
+        clickableIcons={false}
+        mapContainerStyle={{
+          width: "100%",
+          height: "65vh",
+          borderRadius: "10px",
+          boxShadow: "0 8px 16px rgba(0, 0, 0, 0.1)",
+        }}
+        center={
+          activeKeyState.value.length === 0
+            ? cityPositionState.value
+            : directionsState.value?.routes[0]?.legs[0]?.start_location
+        }
+        zoom={zoomLevel}
+        onLoad={(map) => {}}
+      >
+        {cityPositionState.value.lat !== defaultCenter.lat &&
+          cityPositionState.value.lng !== defaultCenter.lng &&
+          activeKeyState.value.length === 0 && (
+            <Marker
+              position={cityPositionState.value}
+              icon={{
+                url: "https://imgur.com/HXGfoxe.png",
+                scaledSize: new window.google.maps.Size(38, 38),
+              }}
+            />
+          )}
+        {activeKeyState.value.length === 1 && (
+          <DirectionsRenderer
+            directions={directionsState.value}
+            options={{ suppressMarkers: true }}
+          />
         )}
+        {cityPositionState.value.lat !== defaultCenter.lat &&
+          cityPositionState.value.lng !== defaultCenter.lng &&
+          activeKeyState.value.length > 0 && (
+            <>
+              {renderMarkerForDay(
+                dayjs(
+                  dayLabels[parseInt(activeKeyState.value[0], 10)],
+                  "DD/MM/YYYY"
+                )
+              ).map((attraction: Attraction, index: number) => {
+                return (
+                  <Marker
+                    key={`${attraction.id}-${index}`}
+                    position={{
+                      lat: attraction.location.latitude,
+                      lng: attraction.location.longitude,
+                    }}
+                    label={{
+                      text: `${(index + 1).toString()}`,
+                      color: "white",
+                      fontWeight: "bold",
+                    }}
+                    icon={{
+                      url: "https://imgur.com/gfrQTuH.png",
+                      scaledSize: new google.maps.Size(38, 38),
+                      labelOrigin: new google.maps.Point(19, 16),
+                    }}
+                    onClick={() => {
+                      selectedMarker === null
+                        ? setSelectedMarker(attraction)
+                        : setSelectedMarker(null);
+                    }}
+                  />
+                );
+              })}
+            </>
+          )}
         {activeKeyState.value.length > 0 && selectedMarker && !imageLoading && (
           <InfoWindow
             options={{ pixelOffset: new google.maps.Size(0, -35) }}
-            position={{ lat: selectedMarker.location.latitude, lng: selectedMarker.location.longitude }}
-            onCloseClick={() => {setSelectedMarker(null)}}
+            position={{
+              lat: selectedMarker.location.latitude,
+              lng: selectedMarker.location.longitude,
+            }}
+            onCloseClick={() => {
+              setSelectedMarker(null);
+            }}
           >
             <div className="attractionContainer">
-              <Image className="attractionImage" src={imageUrl || defaultAttractionImageUrl} alt={selectedMarker.name} preview={false}/>
-              <Title level={5} className="attractionName" style={{ fontWeight: 'bold'}}>{selectedMarker.name}</Title>
-              <Tag icon={<EuroCircleOutlined />}color="green" style={{ gridColumn: '1', gridRow: '2', display: 'inline-block', maxWidth: '60px' }}> {selectedMarker.perPersonCost ? selectedMarker.perPersonCost * (tripState!.value!.nAdults + tripState!.value!.nKids) : "free"}</Tag>
+              <Image
+                className="attractionImage"
+                src={imageUrl || defaultAttractionImageUrl}
+                alt={selectedMarker.name}
+                preview={false}
+              />
+              <Title
+                level={5}
+                className="attractionName"
+                style={{ fontWeight: "bold" }}
+              >
+                {selectedMarker.name}
+              </Title>
+              <Tag
+                icon={<EuroCircleOutlined />}
+                color="green"
+                style={{
+                  gridColumn: "1",
+                  gridRow: "2",
+                  display: "inline-block",
+                  maxWidth: "60px",
+                }}
+              >
+                {" "}
+                {selectedMarker.perPersonCost
+                  ? selectedMarker.perPersonCost *
+                    (tripState!.value!.nAdults + tripState!.value!.nKids)
+                  : "free"}
+              </Tag>
             </div>
           </InfoWindow>
         )}
       </GoogleMap>
     </>
-)};
+  );
+}
 
 export default GoogleMapsComponent;
--- urbanhub/src/components/TripOverview/Sidebar.tsx
+++ urbanhub/src/components/TripOverview/Sidebar.tsx
@@ -1,13 +1,22 @@
-import React from 'react';
-import { Col } from 'react-bootstrap'
-import { Alert, Collapse, CollapseProps, Typography, Button, Flex, Spin, Tooltip } from 'antd';
-import { EditOutlined, EyeOutlined } from '@ant-design/icons';
-import { Trip } from '../../models/trip';
-import colors from '../../style/colors';
-import DailyAttractions from './DailyAttractions';
-import dayjs from 'dayjs';
-import { TripAttraction } from '../../models/tripAttraction';
-import { MessageInstance } from 'antd/es/message/interface';
+import React from "react";
+import { Col } from "react-bootstrap";
+import {
+  Alert,
+  Collapse,
+  CollapseProps,
+  Typography,
+  Button,
+  Flex,
+  Spin,
+  Tooltip,
+} from "antd";
+import { EditOutlined, EyeOutlined } from "@ant-design/icons";
+import { Trip } from "../../models/trip";
+import colors from "../../style/colors";
+import DailyAttractions from "./DailyAttractions";
+import dayjs from "dayjs";
+import { TripAttraction } from "../../models/tripAttraction";
+import { MessageInstance } from "antd/es/message/interface";
 
 const { Title, Paragraph } = Typography;
 
@@ -19,7 +28,7 @@ interface SidebarProps {
   attractionDistances: string[];
   dayLabels: string[];
   editing: {
-    value: boolean
+    value: boolean;
     setter: React.Dispatch<React.SetStateAction<boolean>>;
   };
   errorState: {
@@ -32,13 +41,18 @@ interface SidebarProps {
     setter: React.Dispatch<React.SetStateAction<boolean>>;
   };
   messageApi: MessageInstance;
-  contextHolder: React.ReactElement<any, string | React.JSXElementConstructor<any>>;
+  contextHolder: React.ReactElement<
+    any,
+    string | React.JSXElementConstructor<any>
+  >;
   setDirty: React.Dispatch<React.SetStateAction<boolean>>;
-  setEditingAttraction: React.Dispatch<React.SetStateAction<TripAttraction | null>>;
+  setEditingAttraction: React.Dispatch<
+    React.SetStateAction<TripAttraction | null>
+  >;
   setIsFormVisible: React.Dispatch<React.SetStateAction<boolean>>;
   setMessageAI: React.Dispatch<React.SetStateAction<string>>;
   setSelectedAttractionId: React.Dispatch<React.SetStateAction<string | null>>;
-  setSelectedDay: React.Dispatch<React.SetStateAction<dayjs.Dayjs | null>>; 
+  setSelectedDay: React.Dispatch<React.SetStateAction<dayjs.Dayjs | null>>;
   setUndoVisibility: React.Dispatch<React.SetStateAction<boolean>>;
   travelModel: string;
   trip: Trip | null;
@@ -54,53 +68,74 @@ interface SidebarProps {
 }
 
 function Sidebar(props: SidebarProps) {
-  
-  const { activeKeyState, attractionDistances, dayLabels, editing, form, errorState, loadingState, messageApi,
-          setDirty, setEditingAttraction, setIsFormVisible, setMessageAI, setSelectedAttractionId, 
-          setSelectedDay, setUndoVisibility, travelModel, trip, tripId, tripState,contextHolder, attractionCardHoveredID } = props;
+  const {
+    activeKeyState,
+    attractionDistances,
+    dayLabels,
+    editing,
+    form,
+    errorState,
+    loadingState,
+    messageApi,
+    setDirty,
+    setEditingAttraction,
+    setIsFormVisible,
+    setMessageAI,
+    setSelectedAttractionId,
+    setSelectedDay,
+    setUndoVisibility,
+    travelModel,
+    trip,
+    tripId,
+    tripState,
+    contextHolder,
+    attractionCardHoveredID,
+  } = props;
 
-  const dailyActivities: CollapseProps['items'] = dayLabels.map((dayLabel, index) => ({
-    key: `${index}`,
-    label: dayLabel,
-    children: 
-      <div>
-        <DailyAttractions
-          attractionDistances={attractionDistances}
-          editing={editing.value}
-          form={form}
-          day={dayjs(dayLabel, 'DD/MM/YYYY')}
-          messageApi={messageApi}
-          contextHolder={contextHolder}
-          setDirty={setDirty}
-          setEditingAttraction={setEditingAttraction}
-          setIsFormVisible={setIsFormVisible}
-          setMessageAI={setMessageAI}
-          setSelectedAttractionId={setSelectedAttractionId}
-          setSelectedDay={setSelectedDay}
-          setUndoVisibility={setUndoVisibility}
-          travelModel={travelModel}
-          trip={trip}
-          tripId={tripId}
-          attractionCardHoveredID={{value: attractionCardHoveredID.value, setter: attractionCardHoveredID.setter}}
-        />
-      </div>,
-  }));
+  const dailyActivities: CollapseProps["items"] = dayLabels.map(
+    (dayLabel, index) => ({
+      key: `${index}`,
+      label: dayLabel,
+      children: (
+        <div>
+          <DailyAttractions
+            attractionDistances={attractionDistances}
+            editing={editing.value}
+            form={form}
+            day={dayjs(dayLabel, "DD/MM/YYYY")}
+            messageApi={messageApi}
+            contextHolder={contextHolder}
+            setDirty={setDirty}
+            setEditingAttraction={setEditingAttraction}
+            setIsFormVisible={setIsFormVisible}
+            setMessageAI={setMessageAI}
+            setSelectedAttractionId={setSelectedAttractionId}
+            setSelectedDay={setSelectedDay}
+            setUndoVisibility={setUndoVisibility}
+            travelModel={travelModel}
+            trip={trip}
+            tripId={tripId}
+            attractionCardHoveredID={{
+              value: attractionCardHoveredID.value,
+              setter: attractionCardHoveredID.setter,
+            }}
+          />
+        </div>
+      ),
+    })
+  );
 
   return (
     <>
-      {loadingState.value && 
+      {loadingState.value && (
         <Spin tip="Loading" size="large" fullscreen>
           <div> Loading </div>
         </Spin>
-      }
-      {errorState.value && 
+      )}
+      {errorState.value && (
         <Col>
           <Alert
-            message={
-              <Title level={3}>
-                Oh snap! You got an error!
-              </Title>
-            }
+            message={<Title level={3}>Oh snap! You got an error!</Title>}
             showIcon
             description={
               <>
@@ -109,44 +144,64 @@ function Sidebar(props: SidebarProps) {
               </>
             }
             type="error"
-            style={{width: 'fit-content', margin: 'auto', marginTop: '20px'}}
+            style={{ width: "fit-content", margin: "auto", marginTop: "20px" }}
           />
         </Col>
-      }
+      )}
       {!loadingState.value && !errorState.value && tripState.value && (
         <>
           <div>
-            <Flex style={{ display: 'flex', alignItems: 'center' }}>
-              <Title level={2} className="text-left" style={{ marginRight: '1vw' }}>{tripState.value.city}</Title>
-              { !dayjs(tripState.value.endDate).isBefore(dayjs()) &&
-
-                <Tooltip title={editing.value===true ? "Exit edit mode" : "Enter edit mode"} placement='right'>
-                <Button
+            <Flex style={{ display: "flex", alignItems: "center" }}>
+              <Title
+                level={2}
+                className="text-left"
+                style={{ marginRight: "1vw" }}
+              >
+                {tripState.value.city}
+              </Title>
+              {!dayjs(tripState.value.endDate).isBefore(dayjs()) && (
+                <Tooltip
+                  title={
+                    editing.value === true
+                      ? "Exit edit mode"
+                      : "Enter edit mode"
+                  }
+                  placement="right"
+                >
+                  <Button
                     size="middle"
                     type="primary"
                     className="button-new-trip"
                     style={{
                       backgroundColor: colors.whiteBackgroundColor,
-                      color: 'black',
+                      color: "black",
                       textAlign: "center",
                       fontSize: "15px",
                       marginBottom: "10px",
-                      marginRight: "1vw"
+                      marginRight: "1vw",
                     }}
-                    onClick={() => { !editing.value ?editing.setter(true) : editing.setter(false)}}
+                    onClick={() => {
+                      !editing.value
+                        ? editing.setter(true)
+                        : editing.setter(false);
+                    }}
                   >
                     <span>
-                      {!editing.value ? (<EditOutlined />) : (<EyeOutlined />)}
+                      {!editing.value ? <EditOutlined /> : <EyeOutlined />}
                     </span>
                   </Button>
                 </Tooltip>
-
-              }
-              
-            </Flex>     
+              )}
+            </Flex>
           </div>
-          <div className='sidebar-div'>
-            <Collapse size="large" items={dailyActivities}  accordion={true} activeKey={activeKeyState.value} onChange={(keys) => activeKeyState.setter(keys)}/>
+          <div className="sidebar-div">
+            <Collapse
+              size="large"
+              items={dailyActivities}
+              accordion={true}
+              activeKey={activeKeyState.value}
+              onChange={(keys) => activeKeyState.setter(keys)}
+            />
           </div>
         </>
       )}
