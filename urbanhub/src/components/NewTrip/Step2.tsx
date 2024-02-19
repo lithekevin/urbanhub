c5b000e25c76b57594a6e69fb7a0b6b8d7dc3d5f
0-Elia-0 <126093891+0-Elia-0@users.noreply.github.com>
2024-02-19 11:46:50 +0100

    Preference question changes and progress bar changes

--- urbanhub/src/components/NewTrip/Step2.tsx
+++ urbanhub/src/components/NewTrip/Step2.tsx
@@ -88,7 +88,7 @@ function Step2(props: Step2Props) {
       <Title level={2} className='step-title'> Set your trip preferences </Title>
         <Row className='w-100 d-flex justify-content-center'>
           <Col xs={{span:24}} className='w-100 d-flex flex-column justify-content-center align-items-center mb-4'>
-            <Progress className='w-75' percent={userAnswers.filter(a => a.length !== 0).length * 100/9}  showInfo={false}/>
+            <Progress className='w-75' percent={userAnswers.filter(a => a.length !== 0).length * 100/9}  showInfo={false} strokeColor={(userAnswers.filter(a => a.length !== 0).length * 100/9 < 100) ? colors.softBackgroundColor : colors.hardBackgroundColor}/>
             <Paragraph style={{ fontSize: '13px', textAlign: 'center'}}>{(userAnswers.filter(a => a.length !== 0).length * 100/9 < 100) ? "Keep answering questions until UrbanHub understands the perfect vacation style for you!" : "UrbanHub has understood your ideal vacation style. Click on next to confirm your choices and take a look at the results!"}</Paragraph>
           </Col>
           <Row className='w-100 d-flex justify-content-center'>
