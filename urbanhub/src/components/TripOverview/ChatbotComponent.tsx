7fc90b364ab230c2f1782dcf395e7e782ddf2f71
0-Elia-0 <126093891+0-Elia-0@users.noreply.github.com>
2024-02-21 12:24:38 +0100

    Adding the enter key to chatbot to send data

--- urbanhub/src/components/TripOverview/ChatbotComponent.tsx
+++ urbanhub/src/components/TripOverview/ChatbotComponent.tsx
@@ -93,6 +93,9 @@ function Chatbot(props: ChatbotProps) {
   };
 
   function parseInput(input: string) {
+
+    input = input.trim().replace(/\s+/g, ' ');
+
     const regexDelete = /^(delete|Delete) "(.+)" from (\d{2}\/\d{2}\/\d{4})$/;  //Ex: 'Delete "Attraction to delete" from 12/01/2024'
     const matchDelete = input.match(regexDelete);
     const regexAdd = /^(add|Add) "(.+)" to (\d{2}\/\d{2}\/\d{4}) with time: (\d{2}:\d{2}) - (\d{2}:\d{2})$/; //Ex: 'Add "Attraction to add" to 12/01/2024 with time: 00:00 - 04:00'
@@ -563,6 +566,12 @@ function Chatbot(props: ChatbotProps) {
             placeholder="Ask something to UrbanHub..."
             value={inputValue}
             onChange={handleInputChange}
+            onKeyPress={(event) => {
+              if (event.key === 'Enter') {
+                event.preventDefault(); 
+                handleSendClick();
+              }
+            }}
             autoSize={{ minRows: 1, maxRows: 3 }}
             style={{ marginRight: '10px', width: 'calc(100% - 20px)' }}
           />
@@ -588,7 +597,7 @@ const TypingText = ({ text, reloadText }: { text: string, reloadText: number }) 
       if (index > text.length) {
         clearInterval(timer);
       }
-    }, 50); // Adjust the timing to control the typing speed
+    }, 30); // Adjust the timing to control the typing speed
 
     // Cleanup function
     return () => clearInterval(timer);
