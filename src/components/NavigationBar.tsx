666341f16d8cbd466c2e519b6d102c73fe42820a
lithekevin <130843364+lithekevin@users.noreply.github.com>
2024-08-29 15:57:51 +0100

    fix

--- src/components/NavigationBar.tsx
+++ src/components/NavigationBar.tsx
@@ -10,7 +10,7 @@ function NavigationBar() {
     <Header className="header-style">
     <Row justify="space-between" align="middle" style={{ width: "100%" }}>
       <Col>
-        <Link to="/" style={{ textDecoration: 'none', color: 'black', display: "flex", alignItems: "center" }}>
+        <Link to="/public" style={{ textDecoration: 'none', color: 'black', display: "flex", alignItems: "center" }}>
           <Image src="/logo(1).png" alt="logo" style={{ width: '50px', marginRight: '5px', marginBottom: '4px' }} preview={false}/>
           <Title 
             style={{ marginBottom: 0, marginTop: '6px', position: 'relative', top: '-3px', fontWeight: '600', fontSize: '36px', backgroundImage: 'radial-gradient(circle, #00AA70, #00987e, #008483, #00707e, #185b6f, #2f4858)', color: 'transparent', backgroundClip: 'text' }}
